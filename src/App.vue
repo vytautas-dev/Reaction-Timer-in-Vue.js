a07f4959caf5fa4e4eea98133769f0df0324e5b5
Wojciech Szczygielski <wojciech.szczygielski91@gmail.com>
2022-11-13 20:47:15 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<!-------------------------------------------------SCRIPT---------------------------------------------------->
+<script>
+import Block from "./components/Block.vue";
+import Result from "./components/Result.vue";
+
+export default {
+  name: "App",
+  components: { Result, Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    };
+  },
+  methods: {
+    start() {
+      this.isPlaying = true;
+      this.delay = 2000 + Math.random() * 5000;
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    },
+  },
+};
+</script>
+
+<!-------------------------------------------------TEMPLATE-------------------------------------------------->
+<template>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Result v-if="showResults" :score="score" />
+</template>
+
+<!-------------------------------------------------STYLE----------------------------------------------------->
+<style scoped></style>
---
+++ src/components/Block.vue
@@ -0,0 +1,55 @@
+<!-------------------------------------------------SCRIPT---------------------------------------------------->
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0,
+    };
+  },
+  mounted() {
+    console.log("component mounted");
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
+  },
+  updated() {
+    console.log("component updated");
+  },
+  unmounted() {
+    console.log("unmounted");
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      this.$emit("end", this.reactionTime);
+    },
+  },
+};
+</script>
+
+<!-------------------------------------------------TEMPLATE-------------------------------------------------->
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">click me</div>
+</template>
+
+<!-------------------------------------------------STYLE----------------------------------------------------->
+<style scoped>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
